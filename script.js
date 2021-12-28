093d4683e8008a3dbeb0c382ab33b1d8a5609c77
Ayen11 <einneuman@gmail.com>
2021-12-28 20:58:25 +0200

    added text input and basic popup box and classes

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+
+//set up variables
 let myLibrary = [];
+let titleT = document.querySelector('#titleInput');
+let authorT = document.querySelector('#authorInput');
+let pagesT = 0;
+let readT = false;
+
+
+
+//set up page ref
 const containerRef = document.querySelector("#container");
 
+
+
+const addNewBookBtn = document.querySelector('#addBook'); //button to add books to page
+addNewBookBtn.addEventListener('click', () => {
+    
+    displayBooks();
+    console.log(titleT.value);
+});
+
+
+
+
+
+
+
+//book constructor - also controls info display!
 function book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -18,40 +44,39 @@ function book(title, author, pages, read) {
     }
 };
 
-
+//test of constructor
 const Hobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 const Hobbit2 = new book('The Hobbit2- Tokyo Drift', `J.R.R. Tolkien-2 (He's a robot now!)`, 269, "Read");
 
 
+//simple function to push book object into array. call this before display
 function addBookToLibrary(bookI) {
     myLibrary.push(bookI);
 };
 
 
-//trying to create a new div for every card 
+//adds the cards to the library and from it to the page with class 
 function displayBooks () {
     
-    let gridsize = myLibrary.length;
-    //containerRef.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
-    containerRef.style.gridTemplateRows = `repeat(${gridsize}, 100px)`; //change size of row here
     
+    let gridsize = myLibrary.length;
+    containerRef.style.gridTemplateRows = `repeat(${gridsize}, 100px)`; //change size of row here
+
+    //containerRef.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`; --change Columns here 
 
     for (let i = 0; i < myLibrary.length; i++) {
         const card = document.createElement('div');
         card.classList.add('card');
         card.textContent = myLibrary[i].info();
         containerRef.appendChild(card);
-        //console.log('yo' + i);
         console.log(card.classList);
     };
 };
 
 
+
 //run the commands
 
 
-//addBookToLibrary(Hobbit);
-//console.log(Hobbit.info());
-//console.table(myLibrary);
 addBookToLibrary(Hobbit);
 displayBooks();
